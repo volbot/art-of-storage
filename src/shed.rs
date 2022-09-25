200cbd51da32cb9a27f4ce7d25e4314bc1cc6a45
volbot <lucasmcdermitt@icloud.com>
2022-09-25 00:45:59 -0400

    added plot visibility for sheds

--- src/shed.rs
+++ src/shed.rs
@@ -4,6 +4,30 @@ pub struct ShedPlugin;
 
 impl Plugin for ShedPlugin {
     fn build(&self, app: &mut App) {
-        
+        app.add_startup_system(init_sheds);
+    }
+}
+
+fn init_sheds(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    ) {
+    let plot = Plot::new(Vec3::new(75.,0.,75.));
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 25. })),
+        material: materials.add(Color::rgb(0.5, 0.5, 0.5).into()),
+        transform: Transform::from_translation(plot.pos),
+        ..default()
+    });
+}
+
+pub struct Plot {
+    pos: Vec3,
+}
+
+impl Plot {
+    pub fn new(pos: Vec3) -> Plot {
+        Plot{pos}
     }
 }
