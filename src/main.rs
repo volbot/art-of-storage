1635e3c164e7c685f1f676bbbc151e5c73e3ef87
volbot <lucasmcdermitt@icloud.com>
2022-09-25 00:09:36 -0400

    light shed framework

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use bevy::prelude::*;
 
 pub mod player;
 pub mod world;
+pub mod shed;
 
 use crate::player::PlayerPlugin;
 use crate::world::WorldPlugin;
+use crate::shed::ShedPlugin;
 
 fn main() {
     App::new()
@@ -12,5 +14,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(PlayerPlugin)
         .add_plugin(WorldPlugin)
+        .add_plugin(ShedPlugin)
         .run();
 }
---
+++ src/shed.rs
@@ -0,0 +1,9 @@
+use bevy::prelude::*;
+
+pub struct ShedPlugin;
+
+impl Plugin for ShedPlugin {
+    fn build(&self, app: &mut App) {
+        
+    }
+}
